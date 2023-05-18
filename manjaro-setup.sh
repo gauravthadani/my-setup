02c4c9511ec9debc81a1238999163ef19d828e23
Gaurav Thadani <g.s.thadani@gmail.com>
2023-05-19 09:48:44 +1000

    add granted

--- manjaro-setup.sh
+++ manjaro-setup.sh
@@ -1,4 +1,14 @@
 pamac install xclip --no-confirm
 pamac install vim --no-confirm
 pamac install terminator --no-confirm
+pamac install make --no-confirm
+
+
+echo Installing Granted
+
+curl -OL releases.commonfate.io/granted/v0.11.1/granted_0.11.1_linux_x86_64.tar.gz
+sudo tar -zxvf ./granted_0.11.1_linux_x86_64.tar.gz -C /usr/local/bin/
+
+rm granted_0.11.1_linux_x86_64.tar.gz
+
 
